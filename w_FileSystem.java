4186f3c37ce5616db2f237688bd5897cc7d7c178
AdvaitL <advaitlakhera@gmail.com>
2026-01-26 10:01:09 +0530

    Files System update

--- w_FileSystem.java
+++ w_FileSystem.java
@@ -1,2 +1,26 @@
+import java.nio.file.*;
+import java.io.IOException;
+
 public class w_FileSystem {
+    public static void main(String[] args) {
+        Path path = Paths.get("tasks.json");
+
+        try {
+            // Create file if it doesn't exist
+            if (!Files.exists(path)) {
+                Files.writeString(path, "[]"); // better than createFile
+            }
+
+            // Read file
+            String file = Files.readString(path);
+            System.out.println(file);
+
+            Files.writeString(path, file + "[HIYA]");
+            file = Files.readString(path);
+            System.out.println(file);
+
+        } catch (IOException e) {
+            System.out.println("Failed to access the file");
+        }
+    }
 }
